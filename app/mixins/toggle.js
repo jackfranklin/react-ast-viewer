f57767eeed9833b7d08110ceba1c378def6a9583
Jack Franklin <jack@jackfranklin.net>
2015-03-03 22:29:37 +0000

    old comment

--- app/mixins/toggle.js
+++ app/mixins/toggle.js
@@ -18,8 +18,6 @@ export default {
     emitter.off('toggle-all', this.onToggleAll);
   },
   onToggleClick: function() {
-    //TODO: whenever this gets fired, need to emit event
-    // that mian.js can listen to and change the state of allVisible
     this.setState({ visible: !this.state.visible });
   }
 };
